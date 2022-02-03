f54c343cf55cd21dd24944e50eb08e7b60859ad1
AlexRS90 <aramosbengy@gmail.com>
2022-02-03 10:16:34 -0600

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,13 +1,27 @@
-class Solver 
+class Solver
+  def initialize(input)
+    @input = input
+  end
 
+  def factorial
+    return 'Please choose a number bigger or equal to 0' if @input.negative?
+    return 1 if @input.zero?
 
-    #method 1
+    factorial = 1
+    until @input == 1
+      factorial *= @input
+      @input -= 1
+    end
+    factorial
+  end
 
+  # method 2
 
-
-    #method 2
-
-
-
-    #method 3
+  # method 3
 end
+
+factorial = Solver.new(1)
+factorial2 = Solver.new(0)
+
+puts factorial.factorial
+puts factorial2.factorial
